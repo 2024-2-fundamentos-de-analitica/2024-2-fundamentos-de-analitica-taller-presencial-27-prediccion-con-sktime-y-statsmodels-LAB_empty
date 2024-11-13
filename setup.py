dc56ce3eedcd6a0b40966b9e0e947c6b9bf1d27a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-11-12 21:43:34 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Package installer"""
+
+from setuptools import find_packages, setup  # type: ignore
+
+setup(
+    name="homework",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "ipykernel",
+        "matplotlib",
+        "pandas",
+        "pytest",
+        "seaborn",
+        "sktime",
+        "statsmodels",
+    ],
+)
